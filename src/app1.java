fcfc7204a7556673377c5314620271d90e02ab10
sumit <sumitthundarok@gmail.com>
2023-04-19 00:05:38 +0530

    update

--- src/app1.java
+++
@@ -1,6 +0,0 @@
-
-public class app1 {
-    public static void main(String[] args){
-        System.out.println("Hi my name is sumit");
-    }
-}
--- src/sample.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class sample {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("nextInt() = " + sc.nextInt());
-    System.out.println("nextLine() = " + sc.nextLine());
-  }
-}
